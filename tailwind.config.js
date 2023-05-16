dd9ef250f42555f1630be0efa5b8d9cd873a1243
joshuanelsondev <joshuanelsondev@gmail.com>
2023-05-16 15:58:29 -0400

    Update tailwind color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      black: "#021d28",
+      primary: "#1F7A8C",
+      secondary: "#D1E3DD",
+      blue: "#BFDBF7",
+      gray: "#E1E5F2",
+      white: "#FFFFFF",
+      red: "#E73B3B",
+    },
   },
   plugins: [],
 };
