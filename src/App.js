f91410eb8fc507c9c4b23c39c33f03fe80c54f97
joshuanelsondev <joshuanelsondev@gmail.com>
2023-05-19 19:27:42 -0400

    Edit the Home page

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ function App() {
       </Router>
     </div>
   );
+
 }
 
 export default App;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,12 +3,8 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className="h-full w-full relative">
-      <div className="flex  items-center justify-center w-full h-20 absolute top-8">
-        {/* <Link to={'/'}>
-          <img src={DevStackLogo} alt="DevStack Logo" />
-        </Link> */}
-      </div>
+    <div className="">
+  
     </div>
   );
 }
