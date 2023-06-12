17ae542c65e1ec6ec21bf65bc691066c74fa9ad2
joshuanelsondev <joshuanelsondev@gmail.com>
2023-06-12 15:46:49 -0400

    Edit responsiveness

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -54,10 +54,15 @@ export default function Nav({ toggleMode }) {
       toggleMode();
     };
 
-    const toggleNav = (event) => {
+    const toggleNav = () => {
       setShowNav(!showNav);
-      navigate(`${event.target.id}`)
     };
+    
+    const navAndToggle = (url) => {
+      navigate(url)
+      toggleNav()
+
+    }
 
     const isDarkText = (path) => {
       return location.pathname === path
@@ -70,30 +75,31 @@ export default function Nav({ toggleMode }) {
             src={DevStackLogoLight}
             alt="DevStack Logo"
             className="mt-4 w-52 cursor-pointer"
-            onClick={toggleNav}
+            onClick={() => navigate("/")}
           />
         </div>
         <div className="hidden lg:flex flex-col fixed top-[20%] gap-12 ml-1 sm:ml-4">
           <div className="flex items-center lg:gap-4 md:gap-1">
             <MdHome
-              onClick={toggleNav}
+              onClick={() => navigate("/")}
               className={`peer text-secondary hover:bg-dark rounded-full p-2 cursor-pointer ${
                 isDarkText("/") ? "bg-dark" : "text-secondary"
               }`}
-              id="/"
               size={40}
             />
-            <p className="invisible text-secondary font-semibold peer-hover:visible">
+            <p
+              className="invisible text-secondary font-semibold peer-hover:visible"
+              onClick={() => toggleNav()}
+            >
               Home
             </p>
           </div>
           <div className="flex items-center lg:gap-4 md:gap-1">
             <MdAccountCircle
-              onClick={toggleNav}
+              onClick={() => navigate("/about")}
               className={`peer text-secondary hover:bg-dark rounded-full p-2 cursor-pointer ${
                 isDarkText("/about") ? "bg-dark" : "text-secondary"
               }`}
-              id="/about"
               size={40}
             />
             <p className="invisible text-secondary font-semibold peer-hover:visible">
@@ -102,11 +108,10 @@ export default function Nav({ toggleMode }) {
           </div>
           <div className="flex items-center lg:gap-4 md:gap-1">
             <AiOutlineAppstore
-              onClick={toggleNav}
+              onClick={() => navigate("/projects")}
               className={`peer text-secondary hover:bg-dark p-[8px] rounded-full cursor-pointer ${
                 isDarkText("/projects") ? "bg-dark" : "text-secondary"
               }`}
-              id="/projects"
               size={40}
             />
             <p className="invisible text-secondary font-semibold peer-hover:visible">
@@ -115,11 +120,10 @@ export default function Nav({ toggleMode }) {
           </div>
           <div className="flex items-center lg:gap-4 md:gap-1">
             <AiOutlineAppstoreAdd
-              onClick={toggleNav}
+              onClick={() => navigate("projects/new")}
               className={`peer text-secondary hover:bg-dark rounded-full p-2 cursor-pointer ${
                 isDarkText("/projects/new") ? "bg-dark" : "text-secondary"
               }`}
-              id="/projects/new"
               size={40}
             />
             <p className="invisible text-secondary font-semibold peer-hover:visible">
@@ -128,11 +132,10 @@ export default function Nav({ toggleMode }) {
           </div>
           <div className="flex items-center lg:gap-4 md:gap-1">
             <MdEmail
-              onClick={toggleNav}
+              onClick={() => navigate("/contact")}
               className={`peer text-secondary hover:bg-dark rounded-full p-2 cursor-pointer ${
                 isDarkText("/contact") ? "bg-dark" : "text-secondary"
               }`}
-              id="contact"
               size={40}
             />
             <p className="invisible text-secondary font-semibold peer-hover:visible">
@@ -176,84 +179,74 @@ export default function Nav({ toggleMode }) {
             />
           </div>
           {showNav && (
-            <div className="fixed top-9 ml-20 w-fit pr-1 z-40 bg-primary transition-transform duration-1000 ease-in-out transform translate-y-4 md:hidden">
+            <div className="fixed top-9 ml-20 w-fit pr-1 z-40 bg-primary transition-transform duration-1000 ease-in-out transform translate-y-4 lg:hidden">
               <div
-                className={`flex items-center gap-1 group ${
+                className={`flex items-center gap-1 group cursor-pointer ${
                   isDarkText("/") ? "text-dark" : "text-secondary"
                 }`}
+                onClick={() => navAndToggle("/")}
               >
                 <MdHome
                   className="group-hover:text-dark rounded-full p-2"
                   size={30}
                 />
-                <p
-                  onClick={() => navigate("/")}
-                  className="text-sm font-semibold group-hover:text-dark cursor-pointer hover:scale-110"
-                >
+                <p className="text-sm font-semibold group-hover:text-dark hover:scale-110">
                   Home
                 </p>
               </div>
               <div
-                className={`flex items-center gap-1 group ${
+                className={`flex items-center gap-1 group cursor-pointer ${
                   isDarkText("/about") ? "text-dark" : "text-secondary"
                 }`}
+                onClick={() => navAndToggle("/about")}
               >
                 <MdAccountCircle
                   className="group-hover:text-dark rounded-full p-2"
                   size={30}
                 />
-                <p
-                  onClick={() => navigate("/about")}
-                  className="text-sm font-semibold group-hover:text-dark cursor-pointer hover:scale-110"
-                >
+                <p className="text-sm font-semibold group-hover:text-dark hover:scale-110">
                   About
                 </p>
               </div>
               <div
-                className={`flex items-center gap-1 group ${
+                className={`flex items-center gap-1 group cursor-pointer ${
                   isDarkText("/projects") ? "text-dark" : "text-secondary"
                 }`}
+                onClick={() => navAndToggle("/projects")}
               >
                 <AiOutlineAppstore
                   className="group-hover:text-dark p-[8px] rounded-full"
                   size={30}
                 />
-                <p
-                  onClick={() => navigate("/projects")}
-                  className="text-sm font-semibold group-hover:text-dark cursor-pointer hover:scale-110"
-                >
+                <p className="text-sm font-semibold group-hover:text-dark hover:scale-110">
                   Portfolio
                 </p>
               </div>
               <div
-                className={`flex items-center gap-1 group w-max ${
+                className={`flex items-center gap-1 group w-max cursor-pointer ${
                   isDarkText("/projects/new") ? "text-dark" : "text-secondary"
                 }`}
+                onClick={() => navAndToggle("projects/new")}
               >
                 <AiOutlineAppstoreAdd
                   className="group-hover:text-dark rounded-full p-2"
                   size={30}
                 />
-                <p
-                  onClick={() => navigate("projects/new")}
-                  className="text-sm font-semibold group-hover:text-dark cursor-pointer hover:scale-110"
-                >
+                <p className="text-sm font-semibold group-hover:text-dark hover:scale-110">
                   Add Project
                 </p>
               </div>
               <div
-                className={`flex items-center gap-1 group ${
+                className={`flex items-center gap-1 group cursor-pointer ${
                   isDarkText("/contact") ? "text-dark" : "text-secondary"
                 }`}
+                onClick={() => navAndToggle("/contact")}
               >
                 <MdEmail
                   className="group-hover:text-dark rounded-full p-2"
                   size={30}
                 />
-                <p
-                  onClick={() => navigate("/contact")}
-                  className="text-sm font-semibold group-hover:text-dark cursor-pointer hover:scale-110"
-                >
+                <p className="text-sm font-semibold group-hover:text-dark hover:scale-110">
                   Contact
                 </p>
               </div>
