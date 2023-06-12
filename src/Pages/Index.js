f4bdb539da3ccf4e09a82f86a5dcd0b548bbad3f
joshuanelsondev <joshuanelsondev@gmail.com>
2023-06-12 19:59:47 -0400

    Edit Index responsiveness

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,5 +1,5 @@
 import Projects from "../Components/Projects";
 
 export default function Index() {
-  return <div className="mx-4 pb-10 sm:mx-8 md:mx-48"><Projects /></div>;
+  return <div className="mx-4 pb-10 sm:mx-8 md:mx-48 xl:mx-52 2xl:mx-72"><Projects /></div>;
 }
